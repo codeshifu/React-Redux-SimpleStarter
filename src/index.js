bc237a2e0422757ec6e481dab7fda45023c3be31
Olushi Luqman O <olushilukmon03@gmail.com>
2018-01-22 19:34:23 +0100

    basic setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import {render} from 'react-dom'
 import App from './components/App'
-import 'bootstrap/dist/css/bootstrap.min.css'
-// let's get cracking
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
 
-render(<App />, document.getElementById('root'))
+// styling
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// let's get cracking
+const store = createStore(reducers)
+const boot = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+render(boot, document.getElementById('root'))
