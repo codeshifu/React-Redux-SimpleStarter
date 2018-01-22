93f50ba4f00ffee0f4b674b8c22f6154959de9c9
Olushi Luqman O <olushilukmon03@gmail.com>
2018-01-22 22:14:56 +0100

    wired up redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import {render} from 'react-dom'
 import App from './components/App'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import reducers from './reducers'
 
 // styling
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 // let's get cracking
-const store = createStore(reducers)
+const store = createStore(reducers, applyMiddleware(thunk))
 const boot = (
   <Provider store={store}>
     <App />
